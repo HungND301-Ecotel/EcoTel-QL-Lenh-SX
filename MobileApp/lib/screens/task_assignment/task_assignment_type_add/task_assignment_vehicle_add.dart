db100cae81b9086869fba5d754485da5de52f71c
ecotelDev <dev@ecotel.com.vn>
2025-08-25 11:26:27 +0700

    fix safetymeasure, delete for admin (#31)
    Co-authored-by: dat-nguyen37 <dat958734@gmail.com>

--- MobileApp/lib/screens/task_assignment/task_assignment_type_add/task_assignment_vehicle_add.dart
+++ MobileApp/lib/screens/task_assignment/task_assignment_type_add/task_assignment_vehicle_add.dart
@@ -4,7 +4,6 @@ import 'package:soft/models/device_model.dart';
 import 'package:soft/models/location_model.dart';
 import 'package:soft/models/material_model.dart';
 import 'package:soft/models/order_model.dart';
-import 'package:soft/models/safety_measure_model.dart';
 import 'package:soft/models/shift_model.dart';
 import 'package:soft/models/task_model.dart';
 import 'package:soft/models/user_model.dart';
@@ -20,11 +19,13 @@ import 'package:soft/widgets/vehicle_button.dart';
 class TaskAssignmentVehicleAdd extends StatefulWidget {
   final TaskModel data;
   final OrderModel? order;
+  final String content;
 
   const TaskAssignmentVehicleAdd({
     super.key,
     required this.data,
     this.order,
+    required this.content,
   });
 
   @override
@@ -81,6 +82,7 @@ class _TaskAssignmentVehicleAdd
       _safetyController.text = order.safetyMeasure ?? '';
     } else {
       userAndDevice.add({"user": null, "device": null});
+      _safetyController.text = widget.content;
     }
   }
 
