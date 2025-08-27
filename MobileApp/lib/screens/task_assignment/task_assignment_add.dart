4e10042f6480a43d445c27f3b4d8b5d77bfe369c
dat-nguyen37 <dat958734@gmail.com>
2025-08-27 11:53:36 +0700

    fix remove content in add_task

--- MobileApp/lib/screens/task_assignment/task_assignment_add.dart
+++ MobileApp/lib/screens/task_assignment/task_assignment_add.dart
@@ -39,7 +39,6 @@ class _TaskAssignmentAdd extends State<TaskAssignmentAdd> {
         return TaskAssignmentVehicleAdd(
           data: widget.data,
           order: widget.order,
-          content: content,
         );
       case 'Vận hành xúc':
       case 'Vận hành khoan':
@@ -48,13 +47,11 @@ class _TaskAssignmentAdd extends State<TaskAssignmentAdd> {
         return TaskAssignmentCommonAdd(
           data: widget.data,
           order: widget.order,
-          content: content,
         );
       default:
         return TaskAssignmentOtherAdd(
           data: widget.data,
           order: widget.order,
-          content: content,
         );
     }
   }
@@ -89,8 +86,6 @@ class _TaskAssignmentAdd extends State<TaskAssignmentAdd> {
                 data: widget.data,
                 order: widget.order,
               )
-              : _isLoading
-              ? Center(child: CircularProgressIndicator())
               : _getBody(),
     );
   }
