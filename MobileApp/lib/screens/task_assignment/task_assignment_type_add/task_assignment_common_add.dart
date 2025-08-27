7cb84bdf5d47dd214cc4fc2026bba009d1b47882
dat-nguyen37 <dat958734@gmail.com>
2025-08-27 11:25:50 +0700

    remove content add task

--- MobileApp/lib/screens/task_assignment/task_assignment_type_add/task_assignment_common_add.dart
+++ MobileApp/lib/screens/task_assignment/task_assignment_type_add/task_assignment_common_add.dart
@@ -18,13 +18,11 @@ import 'package:soft/widgets/vehicle_button.dart';
 class TaskAssignmentCommonAdd extends StatefulWidget {
   final TaskModel data;
   final OrderModel? order;
-  final String content;
 
   const TaskAssignmentCommonAdd({
     super.key,
     required this.data,
     this.order,
-    required this.content,
   });
 
   @override
@@ -139,7 +137,6 @@ class _TaskAssignmentCommonAdd
       _noteController.text = order.note ?? '';
     } else {
       userAndDevice.add({"user": null, "device": null});
-      _safetyController.text = widget.content;
     }
   }
 
