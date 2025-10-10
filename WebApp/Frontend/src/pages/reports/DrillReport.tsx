29ab0103a956379e7c42bc4b99d72d5d2cb7eee8
ecotelDev <dev@ecotel.com.vn>
2025-10-10 08:49:26 +0700

    sua hien thi bao cao web (#111)
    Co-authored-by: dat-nguyen37 <dat958734@gmail.com>

--- WebApp/Frontend/src/pages/reports/DrillReport.tsx
+++ WebApp/Frontend/src/pages/reports/DrillReport.tsx
@@ -11,23 +11,41 @@ import {
     Typography
 } from '@mui/material';
 import { format } from 'date-fns';
+import dayjs from 'dayjs';
 import React from 'react';
+import { Department, Shift } from '../../types';
+import { userAtom } from '../../atoms/userAtoms';
+import { useAtom } from 'jotai';
 
 export default function DrillReport({
     data,
     signatureUrl,
     maxTrip,
-    materials
+    materials,
+    startDate,
+    endDate,
+    shifts,
+    department
 }: {
     data: any[]; signatureUrl: string | null,
     maxTrip: number,
-    materials: any[]
+    materials: any[],
+    startDate: dayjs.Dayjs | null,
+    endDate: dayjs.Dayjs | null,
+    shifts: Shift[],
+    department: Department | null
 }) {
+    const [user] = useAtom(userAtom)
 
     return (
         <Grid item xs={12}>
             <Paper sx={{ p: 1 }}>
-                <Typography textAlign={'center'} mb={2} variant='h3'>Báo cáo tổng hợp số liệu trong ca (Máy khoan)</Typography>
+                <i style={{ fontSize: 20 }}>CÔNG TY CỔ PHẦN THAN CAO SƠN-TKV</i>
+                <Typography textAlign={'center'} mb={2} variant='h3' fontWeight={'bold'}>Báo cáo tổng hợp số liệu trong ca (Máy khoan)</Typography>
+                <Typography>Đơn vị: {department ? department.code : user?.department?.code}</Typography>
+                <Typography>Từ ngày: {startDate?.format('DD-MM-YYYY')}</Typography>
+                <Typography>Đến ngày: {endDate?.format('DD-MM-YYYY')}</Typography>
+                <Typography>Ca: {shifts.map(s => s.name).join(', ')}</Typography>
                 <TableContainer sx={{ maxHeight: '80vh' }}>
                     <Table stickyHeader size="small" aria-label="car-trip-report" sx={{
                         '& th, & td': { border: '1px solid black', padding: "2px 8px" }
