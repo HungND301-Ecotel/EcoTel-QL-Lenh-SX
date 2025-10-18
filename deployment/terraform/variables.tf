708ca6ceb3dfc8c4457b2ffa0b34c7b81eef98f9
ecotelTuDV <tudv@ecotel.com.vn>
2025-10-18 17:13:18 +0700

    Adding terraform for CaoSon; Change the S3 bucket structure to latest; daily; Adding restore with specific version

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -5,14 +5,14 @@
 
 variable "customer_it_group_name" {
   description = "Group name of the customer"
-  type    = string
-  default = "IT-Users"
+  type        = string
+  default     = "IT-Users"
 }
 
 variable "customer_it_user_names" {
   description = "name list of the customer "
-  type    = list(string)
-  default = ["it-user1", "it-user2"]
+  type        = list(string)
+  default     = ["it-user1", "it-user2"]
 }
 
 
@@ -44,6 +44,13 @@ variable "backup_bucket_name" {
   default     = "my_bucket"
 }
 
+# Name of the S3 bucket for backups
+variable "s3_backup_object_prefix" {
+  description = "S3 backup object prefix"
+  type        = string
+  default     = "backup/mongodb/databse_name"
+}
+
 # IAM username for backup process
 variable "backup_user_name" {
   description = "IAM user that will be granted access to the backup bucket"
@@ -72,3 +79,13 @@ variable "enable_bucket_encryption" {
   default     = true
 }
 
+
+# List of email recipients for backup alerts
+variable "backup_alert_emails" {
+  description = "Email addresses that will receive backup failure/missing alerts"
+  type        = list(string)
+  default = [
+    "admin@example.com",
+    "it@example.com"
+  ]
+}
