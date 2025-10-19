010c79eacc83f69a29f21260d4484178fe89dca3
ecotelDev <dev@ecotel.com.vn>
2025-10-19 21:30:25 +0700

    sua mo hinh xe (#130)
    Co-authored-by: dat-nguyen37 <dat958734@gmail.com>

--- WebApp/Backend/models/Model.js
+++ WebApp/Backend/models/Model.js
@@ -9,9 +9,17 @@ const Model = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'DeviceModel',
     },
-    value:{
-        type:Number
-    }
+    value: {
+        type: Number
+    },
+    valueHistory: [
+        {
+            value: Number,
+            effectiveDate: {
+                type: Date,
+                default: Date.now,
+            },
+        }],
 },
     {
         timestamps: true
