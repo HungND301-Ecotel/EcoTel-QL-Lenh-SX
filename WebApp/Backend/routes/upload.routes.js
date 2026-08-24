c7a6fbf208f663a159beaa37eb8e82f4005b5c22
Pipeline Worker <pipeline-worker@local>
2026-08-24 07:22:55 +0000

    chore(pipeline-worker): automated run for task #136

--- WebApp/Backend/routes/upload.routes.js
+++ WebApp/Backend/routes/upload.routes.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const handleUpload = require('../utils/uploadImage');
+const handleUpload = require("../utils/handleUpload");
+const { verifyToken } = require("../middleware/auth.middleware");
 
-router.get('/put', handleUpload.getPresignedUrl);
-router.get('/get', handleUpload.getDownloadUrl);
+router.get('/put', verifyToken, handleUpload.getPresignedUrl);
+router.get('/get', verifyToken, handleUpload.getDownloadUrl);
 
 module.exports = router;
